b97ed8356676d40642d8212dcdb20e89bd2f14be
ErPangio <pangilinan.2071368@studenti.uniroma1.it>
2025-05-19 12:29:28 +0200

    test

---
+++ test.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+
+//test 
+int main()
+{
+    printf("hello world");
+    return 0;     //testare se questo funziona
+}
+/*
+va 
+bene amche com i blocchi 
+*/
+
+/*di commenti */
